f19519da28e2c48f8853b9d8ea0625227d498396
rainoffallingstar <3084989256@qq.com>
2022-07-06 16:02:46 +0800

    add survival_analysis

---
+++ static_learning/生存分析.R
@@ -0,0 +1,21 @@
+library(survival)
+data(ovarian)
+str(ovarian)
+colevel <- c(1,2)
+ovarian$resid.ds <- factor(ovarian$resid.ds,
+                           levels = colevel,
+                           labels = c("no","yes"))
+ovarian$rx <- factor(ovarian$rx,
+                     levels = colevel,
+                     labels = c("A","B"))
+ovarian$ecog.ps <- factor(ovarian$ecog.ps,
+                          levels = colevel,
+                          labels = c("good","bad"))
+ovarian$agegr <- cut(ovarian$age,
+                     breaks = c(0,50,75),
+                     labels = c("<=50",">50"))
+surv.obj <- Surv(time = ovarian$futime,event = ovarian$fustat)
+surv.obj
+surv.all <- survfit(surv.obj ~ 1)
+summary(surv.all,censored = TRUE)
+plot(surv.all,mark.time = TRUE)
